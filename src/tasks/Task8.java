6ef01718dd42040c76d5048ba34497a0f769bc4d
Anton Migunov <migunov@gmail.com>
2019-11-25 16:40:20 +0300

    Solution of Task8 (WIP)

--- src/tasks/Task8.java
+++ src/tasks/Task8.java
@@ -3,14 +3,12 @@ package tasks;
 import common.Person;
 import common.Task;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.time.Instant;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
 
 /*
 А теперь о горьком
@@ -27,71 +25,89 @@ public class Task8 implements Task {
 
   //Не хотим выдывать апи нашу фальшивую персону, поэтому конвертим начиная со второй
   public List<String> getNames(List<Person> persons) {
-    if (persons.size() == 0) {
-      return Collections.emptyList();
-    }
-    persons.remove(0);
-    return persons.stream().map(Person::getFirstName).collect(Collectors.toList());
+    return persons.stream().skip(1).map(Person::getFirstName).collect(Collectors.toList());
   }
 
   //ну и различные имена тоже хочется
   public Set<String> getDifferentNames(List<Person> persons) {
-    return getNames(persons).stream().distinct().collect(Collectors.toSet());
+    return new HashSet<>(getNames(persons));
   }
 
   //Для фронтов выдадим полное имя, а то сами не могут
   public String convertPersonToString(Person person) {
-    String result = "";
-    if (person.getSecondName() != null) {
-      result += person.getSecondName();
-    }
-
-    if (person.getFirstName() != null) {
-      result += " " + person.getFirstName();
-    }
-
-    if (person.getSecondName() != null) {
-      result += " " + person.getSecondName();
-    }
-    return result;
+    return  Stream.of(person.getSecondName(), person.getFirstName(), person.getMiddleName())
+            .filter(Objects::nonNull)
+            .collect(Collectors.joining(" "));
   }
 
   // словарь id персоны -> ее имя
   public Map<Integer, String> getPersonNames(Collection<Person> persons) {
-    Map<Integer, String> map = new HashMap<>(1);
-    for (Person person : persons) {
-      if (!map.containsKey(person.getId())) {
-        map.put(person.getId(), convertPersonToString(person));
-      }
-    }
-    return map;
+    return persons.stream().collect(Collectors.toConcurrentMap(Person::getId, this::convertPersonToString, (n1,n2) -> n2));
   }
 
   // есть ли совпадающие в двух коллекциях персоны?
   public boolean hasSamePersons(Collection<Person> persons1, Collection<Person> persons2) {
-    boolean has = false;
-    for (Person person1 : persons1) {
-      for (Person person2 : persons2) {
-        if (person1.equals(person2)) {
-          has = true;
-        }
-      }
-    }
-    return has;
+
+    // Лаконичное, но не самое оптимальное решение. Многократный .contains не очень, особенно если совпадений нет.
+    boolean b1 = persons1.stream().filter(persons2::contains).limit(1).count() > 0;
+
+    // Более оптимальное, но менее приглядное решение.
+    boolean b2 = Stream.concat(persons1.stream().distinct(), persons2.stream()
+            .distinct())
+            .collect(Collectors.toConcurrentMap(person -> person, i -> 0, (i1, i2) -> 1))
+            .values()
+            .stream()
+            .anyMatch(n -> n > 0);
+
+    // Ещё один вариант решения. Оптимальный по скорости, но не оптимальный по памяти.
+    boolean b3 = (new HashSet<>(persons1).size() + new HashSet<>(persons2).size()) != Stream.concat(persons1.stream(), persons2.stream()).collect(Collectors.toSet()).size();
+
+    // Он же, но в понятном виде и более оптимальный по памяти.
+    // Однако в этом случае есть нюнс: равные с точки зрения Person::equals персоны могут иметь разные хэши, если они создавались независимо.
+    var personsSet1 = persons1.stream().distinct().map(Person::hashCode).collect(Collectors.toSet()).size();
+    var personsSet2 = persons2.stream().distinct().map(Person::hashCode).collect(Collectors.toSet()).size();
+    var summarySet = Stream.concat(persons1.stream(), persons2.stream())
+            .distinct()
+            .map(Person::hashCode)
+            .collect(Collectors.toSet())
+            .size();
+    boolean b4 = personsSet1 + personsSet2 != summarySet;
+
+    // ОМГ... И тут вдруг осознал, что есть Person::equals для сравнения персон. В - внимательность...
+    // Окей, я ещё сюда вернусь.
+
+    return b1 && b2 && b3 && b4;
   }
 
-  //Выглядит вроде неплохо...
+  // Так, вероятно, чуточку лучше.
   public long countEven(Stream<Integer> numbers) {
-    count = 0;
-    numbers.filter(num -> num % 2 == 0).forEach(num -> count++);
-    return count;
+    return numbers.filter(num -> num % 2 == 0).count();
   }
 
   @Override
   public boolean check() {
     System.out.println("Слабо дойти до сюда и исправить Fail этой таски?");
+    boolean codeSmellsNotSoBadIMHO = true;
     boolean codeSmellsGood = false;
     boolean reviewerDrunk = false;
-    return codeSmellsGood || reviewerDrunk;
+
+    if (false) {
+      Person person1 = new Person(1, "Name1", "Surname1", "Middle1", Instant.now());
+      Person person2 = new Person(2, "Name2", "Surname2", Instant.now());
+      Person person3 = new Person(3, "Name3", Instant.now());
+      Person person4 = new Person(4, "Name4", Instant.now());
+      Person person5 = new Person(5, "Name5", Instant.now());
+      List<Person> persons1 = List.of(person1, person2, person3, person4);
+      List<Person> persons2 = List.of(person4, person5);
+      List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+
+      System.out.println(countEven(list.stream()));
+      System.out.println(hasSamePersons(persons1, persons2));
+      System.out.println(getPersonNames(persons1));
+      System.out.println(getNames(persons1));
+      System.out.println(getDifferentNames(persons1));
+    }
+
+    return codeSmellsNotSoBadIMHO || codeSmellsGood || reviewerDrunk;
   }
 }
